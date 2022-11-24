d92824964a5470aed84cdfd12701b4fb763497d0
Prajakta Chavan <84058825+Prajakta0412@users.noreply.github.com>
2022-11-24 19:37:02 +0530

    Add files via upload
    commit

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function () {
+    return(
+        <>
+            <div>
+                <i className="fa-regular fa-copyright">
+                    @copyright Auto Tech
+                </i>
+            </div>
+        </>
+    )
+}
---
+++ src/Components/Image.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Image.css';
+
+export default function Image(props) {
+    return(
+        <>
+            <div className="img">
+                <img src="carService.png" alt="" />
+            </div>
+        </>
+    )
+}
